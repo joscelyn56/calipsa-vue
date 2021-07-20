c66f93c0c0655f60ea450c17559c799653484d1b
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2021-07-20 14:30:03 +0100

    Setup Header

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,7 +1,59 @@
 <template>
-  <header/>
+  <header>
+    <div class="logo">
+      <router-link to="/">Calipsa</router-link>
+    </div>
+    <div class="links">
+      <p class="profile">Welcome</p>
+      <p
+        @click="logout"
+        class="logout">Log Out</p>
+    </div>
+  </header>
 </template>
 <script>
 export default {
+  methods: {
+    logout() {
+      this.$store.dispatch('auth/logout').then(() => {
+        this.$router.push({path: '/login'})
+      })
+    }
+  }
 }
 </script>
+<style>
+header {
+  display: flex;
+  padding: 30px 100px;
+  background: #e8f1f9;
+  justify-content: space-between;
+}
+
+.logo {
+  text-transform: uppercase;
+  font-weight: 700;
+}
+
+.logo > a {
+  text-decoration: none;
+  color: #000;
+}
+
+.logo > a:hover {
+  text-decoration: underline;
+}
+
+.profile {
+  font-weight: 600;
+}
+
+.logout {
+  margin-left: 30px;
+  cursor: pointer;
+}
+
+.logout:hover {
+  text-decoration: underline;
+}
+</style>
